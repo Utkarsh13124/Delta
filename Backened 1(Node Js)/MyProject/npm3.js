f0daae24bbdba097c6c4380aa1d304985c65ef65
Utkarsh13124 <utkarshp579@gmail.com>
2025-03-28 20:51:37 +0530

    Backened 1 Completed . Node , npm , export, import packages.

---
+++ Backened 1(Node Js)/MyProject/npm3.js
@@ -0,0 +1,25 @@
+/*
+    require vs import 
+        require krne se wo package ya file aati hi
+        but import krne se hum kisi piece of code ko bhi import kr sakte hi. 
+
+
+    good practice is to either use require or import.
+
+        export item from source
+        recieve at destination 
+            import {sum , PI} from "./math2.js";
+            we also need to name the extension properly. 
+
+
+             but for import few other steps we also need to follow
+                creating package.json file
+                and usme type ko as a module define krna hota hi.
+                    "type" : "module"
+                
+        similarly we can import from package just like we do from files., just we have to install the packages. 
+  
+  
+  
+  
+           */   
---
+++ Backened 1(Node Js)/MyProject/script2.js
@@ -0,0 +1,4 @@
+import {sum , g} from "./math2.js";
+import { generate } from "random-words"; // importing from package.
+console.log(sum(1 , 2));
+console.log(generate());
