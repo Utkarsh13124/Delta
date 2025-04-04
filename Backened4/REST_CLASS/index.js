c7eb5b7834dc8c7bd8f3661a6df16a29944d322a
Utkarsh13124 <utkarshp579@gmail.com>
2025-04-04 15:23:17 +0530

    CRUD operation & Restful apis

---
+++ Backened4/REST_CLASS/index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+// for using UUID package for creating unique id. 
+const { v4 : uuidv4 } = require('uuid');
+    // const { v4: uuidv4 }: This uses destructuring to grab the v4 function from the uuid module and renames it to uuidv4.
+    // v4 ffunction ko hum use kr rhe hi , uuidv4 name se , v4 means version4
+// ! for creating id => uuidv4() it will give a id.
+
+
+// for linking folders
+const path = require("path");
+
+// helping express to understand parse the onject data in requeest body
+    app.use(express.urlencoded({ extended : true }));
+// for using EJS template
+    app.set("view engine" , "ejs");
+    app.set("views" , path.join(__dirname, "views"));
+
+// linking static files from public folder
+    app.use(express.static(path.join(__dirname, "public")));
+
+
+let posts = [
+    {
+        id : uuidv4(),
+        username : "apnaClg",
+        content : "I love Coding."
+    },
+    {
+        id : uuidv4(),
+        username : "Utkarsh",
+        content : "I am covering Backlog"
+    },
+    {
+        id : uuidv4(),
+        username : "Krishna",
+        content : "He love everyone."
+    },
+]
+
+app.listen(port , ()=>{
+    console.log("Listening to port : 8000");
+});
+
+//! index js
+app.get("/posts" , (req, res)=>{
+    res.render("index.ejs" , { posts });
+});
+
+//! post request : - for posting a new post with help of two route
+    app.get("/posts/new" , (req , res) => {
+        res.render("new.ejs");
+    })
+
+    app.post("/posts" , (req, res)=>{
+        // console.log(req.body);
+            // adding the data of req.body to DB / arr
+        let {username , content} = req.body;
+
+        //* giving new Id to post using UUID
+        let id = uuidv4();
+
+        // posts.push({username : username , content : content});
+            //! same naam rehta hi to as key value pass krne ki jarrorat nhi rehti hi.
+        posts.push({id , username , content});
+        // res.send("post req is working.");
+        //! redirecting to all post
+        res.redirect("/posts");
+    })
+
+// Show route :- showing with specific Id.
+    app.get("/posts/:id" , (req , res) => {
+        let {id} = req.params; // id would always be unique , even the username could be same , but id for a post would be different , as a same user is posting again and again
+        
+            //The find method is used to search through the posts array. It returns the first element that satisfies the provided testing function.
+        let post = posts.find( (p) => id === p.id );
+        
+        res.render("show.ejs" , { post });
+    });
