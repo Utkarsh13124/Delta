7d0f9afa2cc01f734f3aac4195f9fc3d51906cb9
Utkarsh13124 <utkarshp579@gmail.com>
2025-04-26 14:05:57 +0530

    Bootstrap Basics is covered

---
+++ BootStrap/theory.js
@@ -0,0 +1,85 @@
+//! What is Bootstap?
+/*
+    just like forms, buttons, etc.
+    
+    Framework & toolkit 
+
+    Syntax ya CSS property yaad krne ki jarrorat nhi hi,
+        wo kaise kaam krti hi , hum kaise apply kr rhe hi , use samajhna important hi ,  
+
+
+        Workson flexbox,
+            havepre-built component.
+
+    What isBootstrap?
+        Bootstrap  is a powerful, featurefrontend toolkit ,build anything - from prototype  toproduction  - in minute.
+
+
+        1. faster development  process.
+        2. prebuiltgood component.
+        3. compatibleto maximum brrowser. 
+        4. responsive 
+
+    We can further modify using vanila css
+
+    
+*/
+
+//! Using Bootstrap
+/*
+
+    - Bootstrap ko download krna
+    - CDN Links
+        Contnet delivery network.
+        network of computers.
+
+    ? write style.csslinkageafter  linkageof bootstrap. 
+        as it will override the style of bootstrap
+
+    Components & Layouts
+        - Component :- button ,  forms
+
+        -Layout 
+            overall  structure , andarrangementsof the element ,
+                - ex - container ,grid
+*/
+
+//! container(layout) & button components
+/*
+    used to contain , pad,
+    .container
+
+    .btn 
+    .btn-outline-success
+
+
+    hum  kisi anchor tag , ko bhi  button ki tarh display kra sakte hi , bus  use btn classdenarhega
+*/
+
+//! badge ( component)
+/*
+    - to create badges
+    - label ko show krna ho ya count ko shoe krna ho.
+
+
+*/
+
+
+
+//! Components
+/*
+
+
+
+*/
+
+//! button Groups
+    /*
+        multiple buttons  kosingle lineme  rakh  skate hi
+        radio buttons ko  bhi group kr sakte hi.  
+    */
+
+//!  navbar
+/*
+    fixed-top class tofixed nav at top
+*/
