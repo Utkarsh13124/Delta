eaeef953c960f918d21fe03b152e2137e2493894
Utkarsh13124 <utkarshp579@gmail.com>
2025-04-30 17:08:37 +0530

    Frontened Validation Added using Bootstrap

---
+++ AirMojo/Phase1_partC.js
@@ -0,0 +1,31 @@
+//! Form Validation 
+/*
+    Done by validation and error handling.
+
+    two types :- 
+        client - side 
+            frontened se jo data backened me ja rha hi , wo shi ho.
+            form se shi data jaye
+
+            should be correct format & within constraint 
+        server - side
+            database ke rule ke hisab se cheeze chl rhi  ya nhi.
+
+
+    Solution to empty form submission :- 
+        jo cheeze form me important unhe sabse pehle required set kr do , jisse empty data bhejne wala system khatam hoo.
+            But this is not standardized , matlab chrome pr dikhata hi , this field is required, safari pr kuch aur dikhayega, brave pr kuch aur
+             hum chahte hi , hum jo bna rhe hi , wo sabke liye same ho.
+             iske liye hum bootstrap ko use krenge.
+              firstly we need to cancel out default setting of browser to add settings for Bootstrap
+              * applied form validation from Docs
+
+
+*/
+
+//! Success & Failure Text
+/*
+    providing some text , before submitting a form
+        like many time when we fill password, it shows strong password , weak password.
+
+*/
---
+++ AirMojo/public/js/script.js
@@ -0,0 +1,22 @@
+//! Client-Side Validation (FORM) --> this could also be pasted in Bolierplate  , or new.ejs
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  
