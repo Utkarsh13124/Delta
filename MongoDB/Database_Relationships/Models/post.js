3b36fa24d6988420e362d3a3286ef83a43c83484
Utkarsh13124 <utkarshp579@gmail.com>
2025-05-02 14:50:09 +0530

    Phase 1 Completed

--- MongoDB/Database_Relationships/Models/post.js
+++ MongoDB/Database_Relationships/Models/post.js
@@ -22,21 +22,55 @@ const postSchema = new Schema({
 });
 
 const User = mongoose.model("User" , userSchema);
-const Post = mongoose.model("Post" , userSchema);
+const Post = mongoose.model("Post" , postSchema);
+
+
 
 const addData = async () => {
-    let user1 = new User({
-        username : "utkarsh",
-        email : "utkarsh@gmai.com",
+    // let user1 = new User({ commenting to prevent again insertion.
+    //     username : "utkarsh",
+    //     email : "utkarsh@gmai.com",
+    // });
+
+    let user = await User.findOne({username : "utkarsh"});
+    
+
+    let post2 = new Post({
+        content : "Bye Bye , 2!",
+        likes : 89,
     });
 
-    let post1 = new Post({
-        content : "hello world!",
-        likes : 8,
-    });
+    post2.user = user; // as hum object ko refer krte hi , but id hi save krega , as per schema design.
 
-    post1.user = user1; // as hum object ko refer krte hi , but id hi save krega , as per schema design.
-
-    await user1.save() ; // saving user before.
-    await post1.save() ;
+    // await user1.save() ; // saving user before.
+    // await post1.save() ;
+    await post2.save() ;
 }
+
+// addData();
+
+const getData = async() => {
+    // let res =  await Post.findOne({}).populate("user"); // jis cheez ko hum print krwana chahte hi, jis property ko . 
+    let res =  await Post.findOne({}).populate("user" , "username"); // specifically printing username for user, as we not want to print all other details.
+    console.log(res);
+};
+
+getData();
+
+
+
+
+// const deletePostById = async (postId) => {
+//     try {
+//       const result = await Post.findByIdAndDelete(postId);
+//       if (result) {
+//         console.log(`Post with ID ${postId} has been deleted.`);
+//       } else {
+//         console.log(`Post with ID ${postId} not found.`);
+//       }
+//     } catch (err) {
+//       console.error('Error deleting post:', err);
+//     }
+//   };
+
+//  deletePostById('68148871a9e4e3c62987edac');
