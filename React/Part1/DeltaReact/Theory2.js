2ceb64518d6cedd6f13ec938b909df464cf25183
Utkarsh13124 <utkarshp579@gmail.com>
2025-06-19 18:30:05 +0530

    Studied React basics

--- React/Part1/DeltaReact/Theory2.js
+++ React/Part1/DeltaReact/Theory2.js
@@ -20,10 +20,41 @@ hum index & main ko same rakhte hi.
         use them as HTML tag
 */
 
+// ! export - import
+/*
+  possible due to webpack, which enable export import.
+  Named export & Default export
+*/
+
+//! Writing markup in JSX
+/*
+  1. return a single root element , final parent would be a single tag.
+  2. closed tag will be used.
+  3. camelcase most of time.
+
+*/
+
+//! React Fragment
+/*
+  <> it combines multiple enity , and his node is not created in DOM</>  
+*/
 
 
+//! JSX with curly braces
+  /*
+    curly braces are used to write pure JS code. 
+  
+  */
 
+    //! Structuring Components
+    /*
+      Ek page ko dekhkr humhe sochna hi , ki kaise hum isse chote chote components me break kr sakte hi.
+    */
 
+//! style Component
+/*
+  Component wiise styling we do generally.
+*/
 
 
 
