460f7d6a6d312c9764e5ffb76b18362c277bbb13
Utkarsh13124 <utkarshp579@gmail.com>
2025-06-19 15:54:02 +0530

    Covering React Part - 1

---
+++ React/Part1/DeltaReact/Theory2.js
@@ -0,0 +1,29 @@
+// flow samjhana is always a key.
+/*
+
+index.html rendere --> main.jsx
+index..html is containing
+
+hum index & main ko same rakhte hi. 
+ */
+
+//! Our 1st Component
+/*
+  resuable & independent code. 
+  
+  Create -> similar like function.
+    return value is html.
+        but only can return
+        one element. 
+
+    Rendering a component
+        use them as HTML tag
+*/
+
+
+
+
+
+
+
+
---
+++ React/Part1/theory.js
@@ -0,0 +1,73 @@
+//! JS Library 
+/*
+    Library for creating UI
+    website react.dev
+
+    Coponents :- individual pieces of code , reusable.
+        imagine like functions. 
+
+    Render krwana --> call krna.
+
+
+    Building components once , used mulutiple times.
+
+
+    React code are in JSX syntax . HTmL in JS . 
+        yaani structure ke ander logic ko add krna.
+
+
+*/
+
+//! What is JSX --> Javascript Extension Syntax.
+/*
+    JSX --> Babel --> JS
+        babel is JS transpile tool
+
+*/
+
+//! set up local environment
+    /*
+        two popular ways 
+            Create-React-App 
+            Vite  ( New and faster )
+
+        vitejs.dev 
+        creating boier plate.
+            1. npm create vite@latest
+                could instal node modules if needed.
+            2. npm run dev
+
+        understanding file structure
+            node module - dependencies.
+            public 
+            src  
+                logic part
+                App.css , App.jsx
+                    app componet is common in every react app 
+                    all component come under app component.
+                index.html 
+                    main page.
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
